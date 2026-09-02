09abfcc65b45acb4cb84317bd73f50fa9da271a1
TomSchippke <tom.schippke12@gmail.com>
2026-09-02 14:53:16 +0200

    Adding RF gridsearch as the first baseline model. Creating other file for futur implementation of transformers and lstm.

---
+++ src/models/baselines.py
@@ -0,0 +1,72 @@
+'''
+This file contains baseline models for the air quality forecasting task.
+They will be used to compare against the more sophisticated models in this project.
+'''
+
+import pandas as pd 
+import os
+import joblib
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+
+def train_random_forest(
+    X_test_train: pd.DataFrame, 
+    y_test_train: pd.DataFrame, 
+    random_state: int = 42,
+    recall: bool = False,
+    model_path: str = "../../results/models/random_forest_best.joblib",
+    param_grid: dict[str, list[int]] = {
+        'n_estimators': [100, 200, 500],
+        'max_depth': [15, 25, None],
+        'min_samples_split': [2, 5],
+        'min_samples_leaf': [1, 2, 4],
+        'max_features': [1.0, 'sqrt']
+    }
+) -> RandomForestRegressor:
+    
+    if recall and os.path.exists(model_path):
+        print(f"=========================================")
+        print(f"Model already existing found here : {model_path}...")
+        print(f"=========================================")
+        return joblib.load(model_path)
+    
+    print(f"=========================================")
+    print(f"Starting random forest grid search...")
+    print(f"=========================================")
+
+    gs = GridSearchCV(
+        estimator=RandomForestRegressor(
+            random_state=random_state,
+            criterion="squared_error"
+        ),
+        param_grid=param_grid,
+        cv=5,
+        refit = True,
+        scoring="neg_root_mean_squared_error",
+        n_jobs=-1,
+        verbose=2
+    )
+        
+    gs.fit(X_test_train, y_test_train)
+    best_rf = gs.best_estimator_
+    best_params = gs.best_params_
+    best_score = gs.best_score_
+    
+    results_df = pd.DataFrame(gs.cv_results_)
+    results_df = results_df.sort_values(by='rank_test_score')
+    results_df.to_csv("../../results/random_forest_grid_search_results.csv", index=False)
+
+    print(f"=========================================")
+    print(f"Best parameters: {best_params}")
+    print(f"Best cross-validation score: {best_score:.3f}")
+    
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    joblib.dump(best_rf, model_path)
+    print(f"Best model saved in : {model_path}")
+    
+    print(f"=========================================")
+    print(f"END of Random Forest training.")
+    
+    return best_rf
+    
+    
---
+++ src/train.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from src.data import (
+    load_and_preprocess, 
+    add_lag_features, 
+    temporal_split, 
+    split_X_y, 
+    scale_features,
+    AirQualityTorchDataset,
+    AirQualityDataLoader
+)
+from src.models.baselines import train_random_forest
+
+
+############################### LOAD AND PROCESS THE DATA ###############################
+
+df = load_and_preprocess(
+    location="Aotizhongxin"
+)
+
+df = add_lag_features(
+    df=df,
+    target="PM2.5",
+    lags=[1,2,3,6,12,24]
+)
+
+train_df, val_df, test_df = temporal_split(df)
+X_train, y_train = split_X_y(train_df)
+X_val, y_val = split_X_y(val_df)
+X_test, y_test = split_X_y(test_df)
+
+X_train, X_val, X_test, scaler = scale_features(X_train, X_val, X_test)
+
+training_data_toch = AirQualityTorchDataset(
+    X=X_train,
+    y=y_train,
+    input_window=48,
+    horizon=6
+)
+
+training_loader = AirQualityDataLoader(
+    dataset=training_data_toch,
+    batch_size=32,
+    shuffle=True
+)
+
+
+############################### MODEL TRAINING ###############################
+
+rf_model = train_random_forest(
+    X_test_train=pd.concat([X_train, X_val], axis=0),
+    y_test_train=pd.concat([y_train, y_val], axis=0),
+    recall=False # turn True to avoid retraining if model already exist 
+)
+
